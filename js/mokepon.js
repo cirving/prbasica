4083d55eba0ea7001bf12711fca9f3d30502b436
Irving <c.irvinggomez@gmail.com>
2024-03-06 14:33:47 -0600

    Primer commit

---
+++ js/mokepon.js
@@ -0,0 +1,26 @@
+function iniciarJuego(){
+    let botonMascotaJugador = document.getElementById("boton-mascota")
+    botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)  
+}
+
+function seleccionarMascotaJugador(){
+
+    let hypodoge = document.getElementById("hypodoge").checked
+    let capipepo = document.getElementById("capipepo").checked
+    let ratigueya = document.getElementById("ratigueya").checked
+    
+    if(hypodoge == true){
+        alert("Has seleccionado a Hypodoge")
+    }else if(capipepo == true){
+        alert("Has seleccionado a Capipepo")
+    }else if(ratigueya == true){
+        alert("Has seleccionado a la Ratigueya")
+    }
+}
+
+window.addEventListener('load', iniciarJuego)
+/*
+Necesitamos los elementos cargados antes de poder utilizarlos,
+por ello se declara la función que escucha el evento de cuando toda 
+la estructura html se ha terminado de cargar. 
+*/
